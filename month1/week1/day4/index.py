14fef4588757b26c6476cf1fb3119fe9ca84807a
riddhi <riddhinashine@gmail.com>
2026-05-22 17:52:04 +0530

    day4modules

---
+++ month1/week1/day4/index.py
@@ -0,0 +1,11 @@
+# import modules as m
+# from modules import find_index,test
+
+import sys
+# sys.path.append("\Users/riddh/OneDrive/Desktop/learning/month1/week1/modules.py")
+from modules import find_index,test
+courses = ['history','math','physics','compsci']
+index = find_index(courses,'math')
+# print(index)
+# print(test)
+print(sys.path)
