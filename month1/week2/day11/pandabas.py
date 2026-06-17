6eeecf8da4146247a1d8b6a727b0d35e8d618a8b
riddhi <riddhinashine@gmail.com>
2026-06-17 20:39:17 +0530

    day11

---
+++ month1/week2/day11/pandabas.py
@@ -0,0 +1,13 @@
+#GROUPING AND SORTING
+import pandas as pd
+data = pd.DataFrame({'name':['riddhi','rohan','shruti','amruta','jashnavi','siddhi'],
+                     'age':[19,19,20,19,20,19],
+                     'surname':['nashine','nandanwar','dhole','gotmare','singdi','gotmare'],
+                     'college':['ghraisoni','ghraisoni','ramdeobaba','ycc','ycc','ramdeobaba'],
+                     'rollno':[7,37,14,1,3,15],
+                     'city':['bhandara','nagpur','nagpur','nagpur','singholi','nagpur']})
+print(data)
+print(data.groupby('city').age.min())
+print(data.groupby('college').apply(lambda df : df['name'].iloc[0]))
+print(data.groupby(['city','age']).apply(lambda df : df.loc[df.college.idxmax()]))
+print(data.groupby('city').age.agg([max,min,len]))
